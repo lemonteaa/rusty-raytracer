2a3e58fce58cb9892d8e3eaa3afb54aa4c03c63a
lemonteaa <lemontea.Tom@gmail.com>
2018-10-10 14:52:35 +0800

    scaffold: output a blank image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate image;
+
+use image::GenericImage;
+use image::Pixel;
+use image::{DynamicImage, Rgba};
+
+use std::fs::File;
+
+fn render() -> DynamicImage {
+    let mut image = DynamicImage::new_rgb8(800, 600);
+    let black = Rgba::from_channels(0, 0, 0, 0);
+    for x in 0..800 {
+        for y in 0..600 {
+            image.put_pixel(x, y, black)
+        }
+    }
+    image
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Learning Raytracer");
+    let img = render();
+    let mut out = File::create("out.png").unwrap();
+    img.write_to(&mut out, image::PNG).expect("Saving image failed");
+    println!("Done. Exiting...");
 }
