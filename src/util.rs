abcaf3bffcf63a42e7c20b54ddfbd19abb7db0cc
lemonteaa <lemontea.Tom@gmail.com>
2018-10-16 21:31:34 +0800

    [minor] color gamma encode

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,34 @@
-use na::Point3;
+use na::{Point3, clamp};
 
 pub struct Color {
-    intensities : Point3<f64>
+    pub intensities : Point3<f64>
+}
+
+pub struct ColorSetting {
+    pub gamma_a : f64,
+    pub gamma_power : f64,
+    pub max_intensity : f64
+}
+
+pub fn gamma_encode(x: f64, a: f64, p: f64) -> f64 {
+    a * x.powf(p)
+}
+
+impl Color {
+    pub fn to_rgb(&self, setting : &ColorSetting) {
+        let r = clamp(gamma_encode(self.intensities.x,
+                    setting.gamma_a, setting.gamma_power),
+                    0.0, setting.max_intensity) / setting.max_intensity;
+        let g = clamp(gamma_encode(self.intensities.y,
+                    setting.gamma_a, setting.gamma_power),
+                    0.0, setting.max_intensity) / setting.max_intensity;
+        let b = clamp(gamma_encode(self.intensities.z,
+                    setting.gamma_a, setting.gamma_power),
+                    0.0, setting.max_intensity) / setting.max_intensity;
+    }
+}
+
+#[test]
+fn test_gamma() {
+    assert_eq!(8.0, gamma_encode(4.0, 0.25, 2.5));
 }
