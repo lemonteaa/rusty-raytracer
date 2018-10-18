7fa7ed12774d3d47c1807ba014685a20cc47aaf3
lemonteaa <lemontea.Tom@gmail.com>
2018-10-18 17:16:25 +0800

    [minor] color arithmetic

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,9 @@
-use na::{Point3, clamp};
+use na::{Vector3, Point3, clamp};
+
+use std::ops::{Add, Mul};
 
 pub struct Color {
-    pub intensities : Point3<f64>
+    pub intensities : Vector3<f64>
 }
 
 pub struct ColorSetting {
@@ -25,6 +27,31 @@ impl Color {
         let b = clamp(gamma_encode(self.intensities.z,
                     setting.gamma_a, setting.gamma_power),
                     0.0, setting.max_intensity) / setting.max_intensity;
+    }
+}
+
+impl Add for Color {
+    type Output = Color;
+    fn add(self, other : Color) -> Color {
+        Color { intensities : self.intensities + other.intensities }
+    }
+}
+
+impl Mul<f64> for Color {
+    type Output = Color;
+    fn mul(self, scalar: f64) -> Color {
+        Color { intensities : scalar * self.intensities }
+    }
+}
+
+impl Mul<Point3<f64>> for Color {
+    type Output = Color;
+    fn mul(self, other: Point3<f64>) -> Color {
+        Color { intensities :
+            Vector3::new(
+                self.intensities.x * other.x,
+                self.intensities.y * other.y,
+                self.intensities.z * other.z) }
     }
 }
 
