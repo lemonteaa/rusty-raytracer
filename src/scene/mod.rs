6e76b18f7dc346f3b63d29c499fce938a72c404e
lemonteaa <lemontea.Tom@gmail.com>
2018-10-19 16:53:49 +0800

    background (I)

--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -1,6 +1,12 @@
 use model::SceneObject;
 use scene::camera::Camera;
 use scene::light::Lighting;
+use util::Color;
+
+use std::path::Path;
+use image::{DynamicImage, open};
+
+use na::Vector3;
 
 pub mod camera;
 pub mod light;
@@ -10,5 +16,24 @@ pub struct Scene {
     pub height: u32,
     pub camera: Camera,
     pub lightins: Vec<Lighting>,
-    pub scene_graph: SceneObject
+    pub scene_graph: SceneObject,
+    pub background: Background
+}
+
+pub enum Background {
+    PlainColor { color : Color },
+    Image { img : DynamicImage }
+}
+
+impl Background {
+    pub fn from_image(img_path: &str) -> Background {
+        Background::Image { img : open(&Path::new(img_path)).unwrap() }
+    }
+
+    pub fn get_pixel(&self, x: u32, y: u32) -> Color {
+        match *self {
+            Background::PlainColor { color } => color,
+            Background::Image { ref img } => Color { intensities: Vector3::zeros() }
+        }
+    }
 }
