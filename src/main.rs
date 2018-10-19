5554756b59d83271e89a1c3e8981f331c2b5539e
lemonteaa <lemontea.Tom@gmail.com>
2018-10-19 22:12:20 +0800

    main render procedure (I)

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,32 @@ use std::fs::File;
 
 use na::{Point3, Vector3, Matrix4};
 
+use scene::Scene;
+use util::{Color, ColorSetting};
+
 pub mod scene;
 pub mod model;
 pub mod util;
 pub mod core;
 
-fn render() -> DynamicImage {
+fn basic_render(scene: Scene, sample_cnt: usize, color_setting: ColorSetting) -> DynamicImage {
+    let mut image = DynamicImage::new_rgb8(scene.width, scene.height);
+    for x in 0..scene.width {
+        for y in 0..scene.height {
+            let mut samples: Vec<Option<Color>> = Vec::with_capacity(sample_cnt);
+            for _ in 0..sample_cnt {
+                let ray = core::gen_ray_antialiased_sample(x, y, &scene);
+                samples.push(core::trace(&ray, &scene.scene_graph, &scene.lightins));
+            }
+            let color = core::antialias_average_color(samples.into_iter(),
+                            scene.background.get_pixel(x, y));
+            image.put_pixel(x, y, color.to_rgb(&color_setting));
+        }
+    }
+    image
+}
+
+fn dummy_render() -> DynamicImage {
     let mut image = DynamicImage::new_rgb8(800, 600);
     let black = Rgba::from_channels(0, 0, 0, 0);
     for x in 0..800 {
@@ -32,7 +52,7 @@ fn render() -> DynamicImage {
 
 fn main() {
     println!("Learning Raytracer");
-    let img = render();
+    let img = dummy_render();
     let mut out = File::create("out.png").unwrap();
     img.write_to(&mut out, image::PNG).expect("Saving image failed");
     println!("Done. Exiting...");
