29ffe5fd6f53d986b65b0063a0d056e218f8f08f
lemonteaa <lemontea.Tom@gmail.com>
2018-10-22 20:43:54 +0800

    generate sample and add progress bar

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ extern crate alga;
 
 extern crate rand;
 
+extern crate indicatif;
+
+use sample::SampleScene;
+use sample::BallScene;
 use image::GenericImage;
 use image::Pixel;
 use image::{DynamicImage, Rgba};
@@ -13,6 +17,8 @@ use image::{DynamicImage, Rgba};
 use std::fs::File;
 
 use na::{Point3, Vector3, Matrix4};
+
+use indicatif::{ProgressBar, ProgressStyle};
 
 use scene::Scene;
 use util::{Color, ColorSetting};
@@ -24,10 +30,11 @@ pub mod core;
 
 pub mod sample;
 
-fn basic_render(scene: Scene, sample_cnt: usize, color_setting: ColorSetting) -> DynamicImage {
+fn basic_render(scene: Scene, sample_cnt: usize, color_setting: ColorSetting,
+    pb: &ProgressBar) -> DynamicImage {
     let mut image = DynamicImage::new_rgb8(scene.width, scene.height);
-    for x in 0..scene.width {
-        for y in 0..scene.height {
+    for y in 0..scene.height {
+        for x in 0..scene.width {
             let mut samples: Vec<Option<Color>> = Vec::with_capacity(sample_cnt);
             for _ in 0..sample_cnt {
                 let ray = core::gen_ray_antialiased_sample(x, y, &scene);
@@ -37,6 +44,7 @@ fn basic_render(scene: Scene, sample_cnt: usize, color_setting: ColorSetting) ->
                             scene.background.get_pixel(x, y));
             image.put_pixel(x, y, color.to_rgb(&color_setting));
         }
+        pb.inc(1);
     }
     image
 }
@@ -54,7 +62,21 @@ fn dummy_render() -> DynamicImage {
 
 fn main() {
     println!("Learning Raytracer");
-    let img = dummy_render();
+    //let img = dummy_render();
+    let test_ball = BallScene {};
+    let test = test_ball.gen_scene();
+
+    let pb = ProgressBar::new(test.height.into());
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{msg} {spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} eta: {eta}")
+        .progress_chars("=> "));
+
+    let img = basic_render(test, 10, ColorSetting {
+        gamma_a: 1.0,
+        gamma_power: 0.25,
+        max_intensity: 1.0
+    }, &pb);
+    pb.finish();
     let mut out = File::create("out.png").unwrap();
     img.write_to(&mut out, image::PNG).expect("Saving image failed");
     println!("Done. Exiting...");
