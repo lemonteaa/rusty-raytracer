2171375dc35134d8937b30a5560f844705b6d0cf
lemonteaa <lemontea.Tom@gmail.com>
2018-10-22 20:43:34 +0800

    [fix] use absolute value in lighting model

--- src/core.rs
+++ src/core.rs
@@ -20,7 +20,7 @@ pub fn lighting_model(intersection : &Intersection,
         let ref l = lighting.light_pos;
         let lc = lighting.color;
         let inc_v = l.get_incident_vec(intersection.intersect_pt);
-        let p = dot(&inc_v, &intersection.normal);
+        let p = dot(&inc_v, &intersection.normal).abs();
         match l {
             LightingType::Ambient =>
                 { c = c + lc * intersection.material.ambient_reflectance },
